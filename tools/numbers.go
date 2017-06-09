352eca7218eba2b7c3671abdd339f0160cad68ee
James <james@mercstudio.com>
2017-06-09 20:39:58 +0800

    parse float64

--- tools/numbers.go
+++ tools/numbers.go
@@ -29,6 +29,18 @@ func ToFixed(num float64, precision int) float64 {
 	return float64(RoundAsInt(num*output)) / output
 }
 
+func ParseFloat64FromString(str string, default_value float64) (float64, error) {
+	if str == "" {
+		return default_value, nil
+	}
+
+	i, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return default_value, err
+	}
+	return i, nil
+}
+
 func ParseIntFromString(str string, default_value int) (int, error) {
 	if str == "" {
 		return default_value, nil
